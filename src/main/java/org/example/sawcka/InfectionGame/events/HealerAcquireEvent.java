32e4fc718d6e4febeb3457cc7bc067d647aaa555
SAwckA <saw.cka@mail.ru>
2024-04-21 00:54:53 +0700

    fix rename despawn effect bug

--- src/main/java/org/example/sawcka/InfectionGame/events/HealerAcquireEvent.java
+++ src/main/java/org/example/sawcka/InfectionGame/events/HealerAcquireEvent.java
@@ -13,16 +13,15 @@ public class HealerAcquireEvent implements Listener {
 
     @EventHandler
     public void onVillagerAcquire(VillagerAcquireTradeEvent event) {
-        Villager villager = (Villager) event.getEntity();
-        if (villager.getName().equals(Healer.getName())) {
-            if(villager.getRecipes().isEmpty()) return;
-            if(villager.getRecipes().get(0) == null) return;
 
-            ItemStack drug = Drug.getDrug();
-            if (villager.getRecipes().get(0).getResult().equals(drug)) {
-                SoundsVisualEffects.healerDeath(villager);
-                villager.damage(villager.getHealth());
-            }
+        Villager villager = (Villager) event.getEntity();
+        if(villager.getRecipes().isEmpty()) return;
+        if(villager.getRecipes().get(0) == null) return;
+
+        ItemStack drug = Drug.getDrug();
+        if (villager.getRecipes().get(0).getResult().equals(drug)) {
+            SoundsVisualEffects.healerDeath(villager);
+            villager.damage(villager.getHealth());
         }
     }
 }
